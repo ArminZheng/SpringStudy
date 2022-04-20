27e585473e0f066bc976e09706545e1b95183ebf
armin <arminzheng@qq.com>
2022-04-20 20:46:11 +0800

    单例bean 延迟加载（对原型bean无效）

--- src/main/java/com/arminzheng/construct/Broker.java
+++ src/main/java/com/arminzheng/construct/Broker.java
@@ -1,6 +1,7 @@
 package com.arminzheng.construct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
  * @version 2022/4/12
  */
 @Component
+@Lazy // 可以使用在类上 方法上
 public class Broker {
 
     private Customer customer;
