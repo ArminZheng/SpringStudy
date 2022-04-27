5dbf581112b914fb7a2bb75c1af3d70f52cd962f
armin <arminzheng@qq.com>
2022-04-27 13:32:32 +0800

    切入点复用

--- src/main/java/com/arminzheng/aspect/MyAspect.java
+++ src/main/java/com/arminzheng/aspect/MyAspect.java
@@ -3,6 +3,7 @@ package com.arminzheng.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * MyAspect
@@ -28,19 +29,44 @@ import org.aspectj.lang.annotation.Aspect;
 public class MyAspect {
 
     /**
-     * 1. 额外功能
-     * 2. 切入点（自动绑定切面）
+     * 1. 额外功能 2. 切入点（自动绑定切面）
+     *
      * @param joinPoint 原始方法
      * @return 返回值
      * @throws Throwable 执行异常
      */
     @Around("execution(* login(..))")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("--------aspect log----------");
+        System.out.println("--------aspect log start----------");
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
             System.out.println("arg = " + arg);
         }
+        System.out.println("--------aspect log end----------");
+        return joinPoint.proceed();
+    }
+
+    /* 切入点复用：切入点配置提取到一个独立的函数上 */
+    /* 函数必须是 public void ; 方法名自定义 */
+    @Pointcut("execution(* login(..))")
+    public void myPointcut() {}
+
+    /**
+     * {@link com.arminzheng.proxy.UserServiceTest#testI() testI}
+     *
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around("myPointcut()")
+    public Object around1(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("--------aspect tx----------");
+        return joinPoint.proceed();
+    }
+
+    @Around("myPointcut()")
+    public Object around2(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("--------aspect performance----------");
         return joinPoint.proceed();
     }
 }
