782d8cd721f0816cf83e2cab46285af22a243c7d
armin <arminzheng@qq.com>
2022-04-27 13:56:20 +0800

    通过ApplicationContextAware得到ctx 再进行内层的方法调用

--- src/main/java/com/arminzheng/proxy/UserServiceImpl.java
+++ src/main/java/com/arminzheng/proxy/UserServiceImpl.java
@@ -1,22 +1,35 @@
 package com.arminzheng.proxy;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * UserServiceImpl
  *
  * @author zy
  * @version 2022/4/20
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Override
     public void register(User user) {
         System.out.println("UserServiceImpl.register 业务运算 + dao 操作");
         // throw new RuntimeException("test exception!");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.login("zhangshan", "wangwu");
     }
 
     @Override
     public boolean login(String name, String password) {
         System.out.println("UserServiceImpl.login");
         return true;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
